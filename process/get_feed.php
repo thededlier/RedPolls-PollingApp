f7c4504bbd90714c354fe15f791b89db8782d6b5
Rohan Anand <thededlier@gmail.com>
2017-03-31 13:35:28 +0530

    text overflow for long descriptions

--- process/get_feed.php
+++ process/get_feed.php
@@ -1,19 +1,19 @@
 <?php
     function getFeed($user) {
         include './process/connect.php';
-        
+
         $html = "";
         $topics = "";
-        
+
         $sql = "SELECT * FROM user_prefs where username = '$user'";
-        
+
         $result = $conn->query($sql);
-        
+
         if($result->num_rows > 0) {
             $row = $result->fetch_assoc();
             $topics = $row["topics"];
         }
-        
+
         $topic = explode(",", $topics);
 
         $sql = "SELECT * FROM polls where topics = '$topic[0]'";
@@ -22,7 +22,7 @@
             $sql .= " OR topics = '$i'";
         }
         $result = $conn->query($sql);
-        
+
         if($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
                 $html .=    '<a href="./events.php?poll_id='. $row["pollID"] . '" class="list-group-item">' .
@@ -32,15 +32,15 @@
                                     '</div>' .
                                     '<div class="col-xs-10 col-md-9">' .
                                         '<h3>' . $row["title"] . '</h3>' .
-                                        '<p>' .
-                                            $row["description"] .   
+                                        '<p class="poll-desc">' .
+                                            $row["description"] .
                                         '</p>' .
                                     '</div>' .
                                 '</div>' .
                             '</a>';
             }
         }
-        
+
         return $html;
     }
 ?>
